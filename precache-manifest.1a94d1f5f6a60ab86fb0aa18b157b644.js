4134e85d7af28d57024e80e7cdf668c04d6604e1
JeGarcia24 <jeralyn.garcia@yahoo.com>
2020-07-31 09:45:14 +0800

    Updates

---
+++ precache-manifest.1a94d1f5f6a60ab86fb0aa18b157b644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69cb5c6a680b44860d00dc4c4c0769d",
+    "url": "/user-finder-app/index.html"
+  },
+  {
+    "revision": "1304e21a131231df607d",
+    "url": "/user-finder-app/static/css/main.889e6b5b.chunk.css"
+  },
+  {
+    "revision": "45d0e38d914eb51df0f1",
+    "url": "/user-finder-app/static/js/2.8465a3a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-finder-app/static/js/2.8465a3a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1304e21a131231df607d",
+    "url": "/user-finder-app/static/js/main.8b23eaf5.chunk.js"
+  },
+  {
+    "revision": "e4fdf47165934725e233",
+    "url": "/user-finder-app/static/js/runtime-main.428a7847.js"
+  }
+]);
